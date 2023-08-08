5d4425159af1b7c60803c74d04786f95f431b7c6
heylarissa <larissah.dandrade@gmail.com>
2023-08-08 01:24:19 -0300

    insert at end singly linked lists

--- c_algorithms/data_structures/singly_linked_lists.c
+++ c_algorithms/data_structures/singly_linked_lists.c
@@ -6,6 +6,26 @@ typedef struct node {
     struct node *next;
 } node_t;
 
+void insert_at_end(node_t **head_ref, int new_data) {
+    node_t * new_node = (node_t *) malloc(sizeof(node_t));
+    node_t * last = *head_ref;
+
+    new_node->value = new_data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    last->next = new_node; // o last é um ponteiro que aponta para o último nodo (passagem por referência)
+    return;
+
+}
 void insert_at_beginnig(node_t **head_list, int new_data){
 
     node_t * new_node = (node_t *) malloc(sizeof(node_t));
@@ -29,7 +49,7 @@ void print_list(node_t **head_list){
 }
 
 int main () {
-    node_t **head_list = (node_t **) malloc(sizeof(node_t));
+    node_t **head_list = (node_t **) malloc(sizeof(node_t)); // referência indireta ao primeiro nodo
 
     insert_at_beginnig(head_list, 2);
     insert_at_beginnig(head_list, 23);
@@ -37,6 +57,9 @@ int main () {
     insert_at_beginnig(head_list, 21);
     insert_at_beginnig(head_list, 29);
 
+    print_list(head_list);
+
+    insert_at_end(head_list, 932);
     print_list(head_list);
 
 }
